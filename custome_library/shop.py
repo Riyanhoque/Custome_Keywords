0c1686bee3e89d49491f499bbc487bbed653f8f2
Thamidul Hoque <thamidulhoque@gmail.com>
2025-04-27 11:59:57 +0300

    #

--- custome_library/shop.py
+++ custome_library/shop.py
@@ -4,10 +4,7 @@ from robot.api.deco import library,keyword
 @library 
 
 class shop():
-
-    def __init__(self):
-        pass
-
+    
     @keyword
     def hello_word(self):
         print("hello")
